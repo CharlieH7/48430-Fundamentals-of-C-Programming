5201446a429bf62ffb0907a715765c0081690029
Charlie Huang <hope91824@gmail.com>
2022-12-07 15:14:12 +1100

    Week 9 content

---
+++ Week 9 - Cross development and Command line parsing/Forum_09/L09a.c
@@ -0,0 +1,14 @@
+/*******************************************************************************
+author     : Beeshanga Abewardana Jayawickrama
+date       :
+
+description:
+
+to show how to use command line arguments. write a program that has two modes of
+operation. two modes are defined by a command line flags:
+    -a : mode a
+    -b : mode b
+If a mode has not been specified by the user or an invalid mode has been
+specified, print an error message.
+*******************************************************************************/
+
